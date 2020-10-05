3cba29aa5d70756810dd678b8bb67ade17b26887
Tyler Gauch <tyler.gauch@tgauch.net>
2020-10-05 12:49:44 -0400

    Updates for google oauth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Amplify, {Auth, Hub} from 'aws-amplify';
+import awsconfig from './aws-exports';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsconfig);
+
+class App extends Component {
+  state = { user: null, customState: null };
+
+  componentDidMount() {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+          this.setState({ user: data });
+          break;
+        case "signOut":
+          this.setState({ user: null });
+          break;
+        case "customOAuthState":
+          this.setState({ customState: data });
+      }
+    });
+
+    Auth.currentAuthenticatedUser()
+      .then(user => this.setState({ user }))
+      .catch(() => console.log("Not signed in"));
+  }
+
+  render() {
+    const { user } = this.state;
+
+    return (
+      <div className="App">
+        <button onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Open Google</button>
+        {user && <button onClick={() => Auth.signOut()}>Sign Out {user.attributes.email}</button>}
+      </div>
+    );
+  }
 }
 
 export default App;
