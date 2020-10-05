9827f24796c89c1464a877dcc31bdb44c8a11556
Tyler Gauch <tyler.gauch@tgauch.net>
2020-10-05 17:31:51 -0400

    Updates to correct url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React, {Component} from 'react';
 import './App.css';
 import Amplify, {Auth, Hub} from 'aws-amplify';
-import awsconfig from './aws-exports';
+import config from './aws-exports';
 
-Amplify.configure(awsconfig);
+// copied from serviceWorker.js to know if it is localhost or not
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+// by default, say it's localhost
+const oauth = {
+  scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+  redirectSignIn: 'http://localhost:3000/',
+  redirectSignOut: 'http://localhost:3000/',
+  responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+};
+
+// if not, update the URLs
+if (!isLocalhost) {
+  oauth.redirectSignIn = 'https://forplusplus.com/';
+  oauth.redirectSignOut = 'https://forplusplus.com/';
+}
+
+// copy the constant config (aws-exports.js) because config is read only.
+var configUpdate = config;
+// update the configUpdate constant with the good URLs
+configUpdate.oauth = {...config.oauth, ...oauth};
+// Configure Amplify with configUpdate
+Amplify.configure(configUpdate);
 
 class App extends Component {
   state = { user: null, customState: null };
@@ -33,7 +63,7 @@ class App extends Component {
     return (
       <div className="App">
         <button onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Open Google</button>
-        {user && <button onClick={() => Auth.signOut()}>Sign Out {user.attributes.email}</button>}
+        {user && <button onClick={() => Auth.signOut()}>Sign Out {user.getUsername()}</button>}
       </div>
     );
   }
