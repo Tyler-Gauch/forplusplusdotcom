829ddfba028581776a2bcb6f271fd614844d16b7
Tyler Gauch <tyler.gauch@tgauch.net>
2020-10-09 22:51:12 -0400

    fixed fontawesome deps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './js/App';
 
--- src/js/components/NavigationBar.js
+++ src/js/components/NavigationBar.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faYoutube} from "@fortawesome/free-brands-svg-icons"
 
 class NavigationBar extends Component {
   state = {
@@ -21,14 +23,16 @@ class NavigationBar extends Component {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
           </Nav>
         </Navbar.Collapse>
         <Navbar.Collapse className="justify-content-end">
               {this.props.userAttributes
                 ?
                   <NavDropdown title={this.props.userAttributes.name} id="my-account-dropdown">
-                    <NavDropdown.Item href="#">Subscribe</NavDropdown.Item>
+                    <NavDropdown.Item href="#">
+                        <Fragment>Subscribe </Fragment>
+                        <FontAwesomeIcon icon={faYoutube} />
+                    </NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#" onClick={this.props.logoutCallback}>Logout</NavDropdown.Item>
                   </NavDropdown>
